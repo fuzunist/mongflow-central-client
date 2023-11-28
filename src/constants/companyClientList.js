b46211af5bea1f61b82fc57b360ddf8600845f2a
elif <elifdeniz@elif-MBP.home>
2023-11-28 16:07:52 +0300

    firm clients links added

--- src/constants/companyClientList.js
+++ src/constants/companyClientList.js
@@ -1,4 +1,4 @@
 export const companyClientList = {
-  125869: "http://localhost:5175",
-  175436: "http://localhost:5174",
+  125869: "https://mongflow-ata-client.onrender.com",
+  175436: "https://mongflow-frontend-v1.onrender.com",
 };
