20dc7d43a56ea11cd00d68f675b9758129c35e63
elif <elifdeniz@elif-MacBook-Pro.local>
2023-11-29 11:22:46 +0300

    route fix3

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,59 +1,38 @@
-import { createBrowserRouter } from 'react-router-dom'
+import { createBrowserRouter } from "react-router-dom";
 // import Root from './Root'
-import NotFound from '../pages/not-found'
-import Root from './Root'
+import NotFound from "../pages/not-found";
+import Root from "./Root";
 
 //layouts
-import AuthLayout from '../layouts/AuthLayout'
+import AuthLayout from "../layouts/AuthLayout";
 
-import Welcome from '../pages/Welcome'
-import Login from '../pages/Login'
+import Welcome from "../pages/Welcome";
+import Login from "../pages/Login";
 // import Register from '../pages/Register'
 // import ForgetPassword from "../pages/ForgetPassword"
 // import LogOut from '@/pages/LogOut'
 // import ForgetPassword from '@/pages/ForgetPassword'
 
-
 const routes = createBrowserRouter([
-    {
-        path: '/',
-        element: <AuthLayout />,
-        errorElement: <NotFound />,
-        children:[
-            {
-                index: true,
-                path: "auth/login",
-                element: <Login />,
-                errorElement: <NotFound />
-            }
-        ]
-    },
-    // {
-    //     path: '*',
-    //     element: <AuthLayout />,
-    //     children: [
-    //         {
-    //             path: 'auth/login',
-    //             element: <Login />,
-    //             errorElement: <NotFound />
-    //         },
-    //         // {
-    //         //     path: 'register',
-    //         //     element: <Register />,
-    //         //     errorElement: <NotFound />
-    //         // },
-    //         // {
-    //         //     path: 'forget-password',
-    //         //     element: <ForgetPassword />,
-    //         //     errorElement: <NotFound />
-    //         // },
-    //         // {
-    //         //     path: 'logout',
-    //         //     element: <LogOut />,
-    //         //     errorElement: <NotFound />
-    //         // }
-    //     ]
-    // }
-])
+  {
+    path: "/",
+    element: <Root />,
+    errorElement: <NotFound />,
+  },
+  {
+    path: "auth",
+    element: <AuthLayout />,
+    children: [
+      {
+        path: "login",
+        element: <Login />,
+      },
+    ],
+  },
+  {
+    path: "*",
+    element: <NotFound />,
+  },
+]);
 
-export default routes
+export default routes;
