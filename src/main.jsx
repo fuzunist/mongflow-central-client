038eaa94d59b290af9f6badec3972b4b5dfa5ec2
elif <elifdeniz@elif-MacBook-Pro.local>
2023-11-29 11:50:59 +0300

    route fix

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,20 @@
+import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+import "./assets/css/globals.css";
+import { RouterProvider } from "react-router-dom";
+import routes from "./routes";
+import store from "./store";
+import "./i18n";
+import { BrowserRouter as Router, Route, Redirect } from "react-router-dom";
+import Login from "../pages/Login";
 
-import ReactDOM from 'react-dom/client'
-import { Provider } from 'react-redux'
-import './assets/css/globals.css'
-import { RouterProvider } from 'react-router-dom'
-import routes from './routes'
-import store from './store'
-import './i18n'
-
-
-ReactDOM.createRoot(document.getElementById('root')).render(
- <Provider store={store}>
-        <RouterProvider router={routes} />
-</Provider>
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <Provider store={store}>
+    <Router>
+      <Route exact path="/">
+        <Redirect to="/auth/login" />
+      </Route>
+      <Route path="/auth/login" component={Login} />
+    </Router>
+  </Provider>
+);
