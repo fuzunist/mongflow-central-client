8e43085703194c7a514ef8770adcb0b320abbf9c
elif <elifdeniz@elif-MacBook-Pro.local>
2023-11-29 15:32:36 +0300

    cookie set

--- src/services/auth.js
+++ src/services/auth.js
@@ -1,26 +1,33 @@
-import axios from 'axios'
+import axios from "axios";
 
 export const verify = async (access_token) => {
-    try {
-        const { data } = await axios.get(`${import.meta.env.VITE_API_ENDPOINT}/user`, {
-            headers: {
-                Authorization: `Bearer ${access_token}`
-            }
-        })
-        return data
-    } catch (e) {
-        return e.response.data
-    }
-}
-
+  try {
+    const { data } = await axios.get(
+      `${import.meta.env.VITE_API_ENDPOINT}/user`,
+      {
+        headers: {
+          Authorization: `Bearer ${access_token}`,
+        },
+      }
+    );
+    return data;
+  } catch (e) {
+    return e.response.data;
+  }
+};
 
 export const login = async (Email, Password) => {
-    try {
-         console.log(import.meta.env.VITE_API_ENDPOINT)
-        const { data } = await axios.post(`${import.meta.env.VITE_API_ENDPOINT}/user/login`, { Email, Password })
-        return data
-    } catch (e) {
-        return e.response.data
-    }
-}
-
+  try {
+    console.log(import.meta.env.VITE_API_ENDPOINT);
+    const { data } = await axios.post(
+      `${import.meta.env.VITE_API_ENDPOINT}/user/login`,
+      { Email, Password },
+      {
+        withCredentials: true, // Allow cookies to be sent
+      }
+    );
+    return data;
+  } catch (e) {
+    return e.response.data;
+  }
+};
