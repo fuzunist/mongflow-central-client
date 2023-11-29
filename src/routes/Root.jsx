f040cf00cbda988d25d9a49f1c7ba82d8c17655e
elif <elifdeniz@elif-MacBook-Pro.local>
2023-11-29 11:01:23 +0300

    not found err fixed

--- src/routes/Root.jsx
+++ src/routes/Root.jsx
@@ -1,29 +1,13 @@
 import { useEffect } from "react";
-import { useCookies } from "react-cookie";
-import { useNavigate, Navigate } from "react-router-dom";
-
-import { verify } from "../services/auth";
-import { setUser } from "../store/actions/user";
-// import { promiseAll } from '@/store/actions/apps'
-
 import Loader from "@/components/Loader";
 
 const Root = () => {
-  const [cookies] = useCookies(["access_token", "refresh_token"]);
-  const navigate = useNavigate();
-
-  const verifyHandle = async () => {
-    const beforePathname = sessionStorage.getItem("beforePathname");
-    console.log("verifyHandle - beforePathnam: ", beforePathname);
-
-    if (!cookies?.access_token) return navigate("/auth/login");
-    const response = await verify(cookies?.access_token);
-    if (response?.error) return navigate("/auth/login");
-    navigate("/auth/login" ?? beforePathname);
+  const redirectHandle = async () => {
+    window.location.href = "/auth/login";
   };
 
   useEffect(() => {
-    verifyHandle();
+    redirectHandle();
   }, []);
 
   return <Loader />;
